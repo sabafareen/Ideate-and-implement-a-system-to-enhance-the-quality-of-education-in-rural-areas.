00a7b84f1933ef6857198455749dd939d06bc2ae
sabafareen <161591009+sabafareen@users.noreply.github.com>
2025-12-02 23:51:48 +0530

    NormalFragment.java

---
+++ my_trail/app/src/java/NormalFragment.java
@@ -0,0 +1,41 @@
+package com.example.mytrail1;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NormalFragment extends Fragment {
+
+    public NormalFragment() {
+       
+    }
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        
+
+        View view = inflater.inflate(R.layout.fragment_normal, container, false);
+
+        view.findViewById(R.id.textView).setOnClickListener(v -> changeFragment(new CategoryFragment()));
+
+        return view;
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+        fragmentTransaction.addToBackStack(null); 
+        fragmentTransaction.commit();
+    }
+}
