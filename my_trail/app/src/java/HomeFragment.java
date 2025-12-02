7a80fecf484fbbe0932d4a4947eb95bfdede66a3
sabafareen <161591009+sabafareen@users.noreply.github.com>
2025-12-02 23:44:58 +0530

    HomeFragment.java

---
+++ my_trail/app/src/java/HomeFragment.java
@@ -0,0 +1,41 @@
+package com.example.mytrail1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+       
+        view.findViewById(R.id.imageN1).setOnClickListener(v -> changeFragment(new Video1Fragment()));
+        view.findViewById(R.id.imageN2).setOnClickListener(v -> changeFragment(new Video2Fragment()));
+
+
+        return view;
+    }
+
+
+    private void changeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+        fragmentTransaction.addToBackStack(null); 
+        fragmentTransaction.commit();
+    }
+}
